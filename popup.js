94efaad977b33f87701c7caf2328dbd096a7f67b
jfobrien29 <thejackobrien29@gmail.com>
2019-09-13 00:06:31 -0400

    Chome plugin can save links

--- popup.js
+++ popup.js
@@ -1,14 +1,25 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var checkPageButton = document.getElementById('savePage');
-    checkPageButton.addEventListener('click', function() {
-  
-      chrome.tabs.getSelected(null, function(tab) {
-        d = document;
+document.addEventListener('DOMContentLoaded', function () {
+  var checkPageButton = document.getElementById('savePage');
+  checkPageButton.addEventListener('click', function () {
 
-        const time = document.getElementById('numberPicker').value;
-
-        const text = `Took ${time}m to read ${tab.url}`; 
-        document.getElementById('content').textContent = text;
+    chrome.tabs.getSelected(null, function (tab) {
+      const time = document.getElementById('numberPicker').value;
+      var data = JSON.stringify({
+        "link": tab.url,
+        "timeToRead": time,
       });
-    }, false);
+
+      var Http = new XMLHttpRequest();
+      Http.open("POST", "http://localhost:3000/link");
+      Http.setRequestHeader("Content-Type", "application/json");
+      Http.setRequestHeader("Cache-Control", "no-cache");
+
+      Http.send(data);
+
+      Http.onreadystatechange = (e) => {
+        const text = `Took ${time}m to read ${tab.url}. Saved Successfully!!`;
+        document.getElementById('content').textContent = text;
+      }
+    });
   }, false);
+}, false);
